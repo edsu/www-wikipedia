027bed5e659bac7b38235c51bc65090c30eeb228
Brian Cassidy <bricas@cpan.org>
2006-11-13 18:51:58 +0000

    added random() method
    git-svn-id: http://inkdroid.org/svn/www-wikipedia/trunk@596 4dc5e89f-90f6-0310-ab54-a6a856e7c30e

--- lib/WWW/Wikipedia.pm
+++ lib/WWW/Wikipedia.pm
@@ -12,6 +12,8 @@ our $VERSION = '1.9';
 
 use constant WIKIPEDIA_URL =>
     'http://%s.wikipedia.org/w/index.php?title=%s&action=raw';
+use constant WIKIPEDIA_RAND_URL =>
+    'http://%s.wikipedia.org/wiki/Special:Random';
 
 =head1 NAME
 
@@ -131,6 +133,28 @@ sub search {
 	return undef;
     }
 
+}
+
+=head2 random()
+
+This method fetches a random wikipedia page.
+
+=cut
+
+sub random {
+    my( $self )  = @_;
+    my $src      = sprintf( WIKIPEDIA_RAND_URL, $self->language() );
+    my $response = $self->get( $src );
+
+    if( $response->is_success() ) {
+        # get the raw version of the current url
+        $src = $response->request->uri . '?action=raw';
+        $response = $self->get( $src );
+        return WWW::Wikipedia::Entry->new( $response->content(), $src );
+    }
+
+    $self->error( "uhoh, WWW::Wikipedia unable to contact " . $src );
+    return;
 }
 
 =head2 error()
---
+++ t/21.random.t
@@ -0,0 +1,13 @@
+use Test::More tests => 4;
+
+use WWW::Wikipedia;
+
+# test default language
+my $wiki = WWW::Wikipedia->new();
+isa_ok( $wiki, 'WWW::Wikipedia' );
+
+my $entry = $wiki->random();
+isa_ok( $entry, 'WWW::Wikipedia::Entry' );
+ok( length($entry->text()) > 0, 'text()' );
+ok( length($entry->fulltext()) > 0, 'fulltext()' );
+
