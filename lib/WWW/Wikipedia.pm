4f94dfa8c14e1a24de466639b174d559f770cc99
Brian Cassidy <brian.cassidy@gmail.com>
2015-12-21 23:13:03 -0400

    prep release

--- lib/WWW/Wikipedia.pm
+++ lib/WWW/Wikipedia.pm
@@ -280,7 +280,7 @@ Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003-2013 by Ed Summers
+Copyright 2003-2015 by Ed Summers
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
